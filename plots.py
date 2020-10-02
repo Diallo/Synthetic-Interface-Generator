129a52281b339c9f438da5708e36f38acd83b52f
Diallo <mnl.diallo@gmail.com>
2020-10-02 15:45:40 +0100

    added plots

--- plots.py
+++ plots.py
@@ -7,121 +7,229 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 from matplotlib.pyplot import figure
-figure(num=None, figsize=(18, 6), dpi=80, facecolor='w', edgecolor='k')
+import random
+
+
+
+def make_figure(name="results/experiments{}",number=1):
+    filename = name.format(number) + ".csv"
+    fig = figure(num=None, figsize=(18, 6), dpi=80, facecolor='w', edgecolor='k')
+    fig.suptitle('Timeout 350secs | 0.2 prevalence', fontsize=16)
+
+
+
+    create = defaultdict(list)
+    delete = defaultdict(list)
+    merge = defaultdict(list)
+    split = defaultdict(list)
+
+
+    with open(filename, "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        for _, line in enumerate(reader):
+            oper,ins,outs,prev,t,status = line
+            selected = None
+            if "delete" in oper:
+                selected = delete
+            if "create" in oper:
+                selected = create
+            if "merge" in oper:
+                selected = merge
+            if "split" in oper:
+                selected = split
+
+            
+
+            selected["{},{}".format(ins,outs)].append(float(t))
+
+    for key,value in create.items():
+        create[key] = sum(value) / len(value)
+    
+    for key,value in delete.items():
+        delete[key] = sum(value) / len(value)
+    
+    for key,value in merge.items():
+        merge[key] = sum(value) / len(value)
+    
+    for key,value in split.items():
+        split[key] = sum(value) / len(value)
+    
+
+    plt.subplot(2, 2, 1)
+    plt.bar(list(create.keys()), list(create.values()),label="Create")
+    plt.axhline(y=350,linewidth=1, color='red',label="Timeout")
+    # # plt.yscale("symlog")
+    plt.ylabel("T in seconds")
+    plt.xlabel("Inputs, Outputs")
+    plt.ylim(-10, 400)
+    plt.title("Create")
+
+    plt.subplot(2, 2, 2)
+    plt.bar(list(delete.keys()), list(delete.values()))
+    # # plt.yscale("symlog")
+    plt.title("Delete")
+    plt.axhline(y=350,linewidth=1, color='red',label="Timeout")
+    plt.ylim(-10, 400)
+    plt.ylabel("T in seconds")
+    plt.xlabel("Inputs, Outputs")
+
+
+
+    plt.subplot(2, 2, 3)
+    plt.bar(list(merge.keys()), list(merge.values()))
+    # # plt.yscale("symlog")
+    plt.title("merge")
+    plt.axhline(y=350,linewidth=1, color='red',label="Timeout")
+    plt.ylim(-10, 400)
+    plt.ylabel("T in seconds")
+    plt.xlabel("Inputs, Outputs")
+
+
+    plt.subplot(2, 2, 4)
+    plt.bar(list(split.keys()), list(split.values()))
+    # # plt.yscale("symlog")
+    plt.title("split")
+    plt.axhline(y=350,linewidth=1, color='red',label="Timeout")
+    plt.ylim(-10, 400)
+    plt.ylabel("T in seconds")
+    plt.xlabel("Inputs, Outputs")
 
 
 
 
-create = defaultdict(list)
-delete = defaultdict(list)
-merge = defaultdict(list)
-split = defaultdict(list)
+    
+    plt.tight_layout()
+
+    plt.savefig(name.format(number) + ".png")
 
 
-with open("results/equal_result_multipel_mod.csv", "r") as f:
-    reader = csv.reader(f, delimiter=",")
-    for i, line in enumerate(reader):
-        oper,ins,outs,prev,t = line
-        selected = None
-        if oper == "delete.delete.delete":
-            selected = delete
-        if oper == "create.create.create":
-            selected = create
-        if oper == "merge.merge.merge":
-            selected = merge
-        if oper == "split.split.split":
-            selected = split
+def make_scatter(name="results/experiments{}",number=1):
+    filename = name.format(number) + ".csv"
+    fig = figure(num=None, figsize=(18, 6), dpi=80, facecolor='w', edgecolor='k')
+    fig.suptitle('Timeout 350secs | 0.2 prevalence', fontsize=16)
 
 
-        
-        selected["{},{}".format(ins,outs)].append(float(t))
 
-for key,value in create.items():
-    create[key] = sum(value) / len(value)
+    create = defaultdict(list)
+    delete = defaultdict(list)
+    merge = defaultdict(list)
+    split = defaultdict(list)
+
+
+    with open(filename, "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        for _, line in enumerate(reader):
+            oper,ins,outs,prev,t,status = line
+            selected = None
+            if "delete" in oper:
+                selected = delete
+            if "create" in oper:
+                selected = create
+            if "merge" in oper:
+                selected = merge
+            if "split" in oper:
+                selected = split
+
+            
+            if float(t) < 0:
+                t = -50
+
+            selected["{}.{}".format(ins,outs)].append(float(t))
+
    
-for key,value in delete.items():
-    delete[key] = sum(value) / len(value)
-   
-for key,value in merge.items():
-    merge[key] = sum(value) / len(value)
-   
-for key,value in split.items():
-    split[key] = sum(value) / len(value)
-   
+    
 
-plt.subplot(2, 2, 1)
-plt.bar(list(create.keys()), list(create.values()),label="Create")
-plt.axhline(y=0,linewidth=1, color='red')
-# plt.yscale("log")
-plt.ylim(-10, 300)
-plt.title("Create")
+    plt.subplot(2, 2, 1)
 
-plt.subplot(2, 2, 2)
-plt.bar(list(delete.keys()), list(delete.values()))
-# plt.yscale("log")
-plt.title("Delete")
-plt.axhline(y=0,linewidth=1, color='red')
-plt.ylim(-10, 300)
+    for key,value in create.items():
+        i = 0
+        for val in value:
+            if val < 0:
+                val -= 30 * i 
+                i += 1
+            plt.scatter(key,val)
 
-
-plt.subplot(2, 2, 3)
-plt.bar(list(merge.keys()), list(merge.values()))
-# plt.yscale("log")
-plt.title("merge")
-plt.axhline(y=0,linewidth=1, color='red')
-plt.ylim(-10, 300)
-
-plt.subplot(2, 2, 4)
-plt.bar(list(split.keys()), list(split.values()))
-# plt.yscale("log")
-plt.title("split")
-plt.axhline(y=0,linewidth=1, color='red')
-plt.ylim(-10, 300)
+    plt.axhline(y=0,linewidth=1, color='black',label="zero")
+    # plt.yscale("symlog")
+    plt.ylabel("T in seconds")
+    plt.xlabel("Inputs, Outputs")
+    plt.xticks(["10,30,50,80"])
+    plt.ylim(-150, 400)
+    plt.title("Create")
 
 
 
+    plt.subplot(2, 2, 2)
+    for key,value in delete.items():
+        i = 0
+        for val in value:
+            if val < 0:
+                val -= 30 * i 
+                i += 1
+            plt.scatter(key,val)
+    # plt.yscale("symlog")
+    plt.title("Delete")
+    plt.axhline(y=0,linewidth=1, color='black',label="zero")
+    plt.ylabel("T in seconds")
+    # plt.xticks([10,30,50,80])
+    plt.ylim(-150, 400)
 
-plt.tight_layout()
+    plt.xlabel("Inputs, Outputs")
 
-plt.savefig("different_out.png")
 
-exit(1)
 
-# labels = ["1,5","5,5"]
-# create = [251,-10]
-# delete = [2,315]
-# merge = [1, 17]
-# split = [4, -10]
-# # Set position of bar on X axis
-# r1 = np.arange(len(create))
-# r2 = [x + barWidth for x in r1]
-# r3 = [x + barWidth for x in r2]
-# r4 = [x + barWidth for x in r3]
- 
-# # Make the plot
-# rects1 = ax.bar(r1, create, width=barWidth, edgecolor='white', label='create')
-# rects2 = ax.bar(r2, delete, width=barWidth, edgecolor='white', label='delete')
-# rects3 = ax.bar(r3, merge, width=barWidth, edgecolor='white', label='merge')
-# rects4 = ax.bar(r4, split, width=barWidth, edgecolor='white', label='split')
- 
-# # Add xticks on the middle of the group bars
-# plt.xlabel('Inputs, Outputs', fontweight='bold')
-# plt.xticks([r + barWidth for r in range(len(create))], ['1,5', '5,5', ])
- 
-# # Create legend & Show graphic
-# plt.legend()
-# def autolabel(rects):
-#     """Attach a text label above each bar in *rects*, displaying its height."""
-#     for rect in rects:
-#         height = rect.get_height()
-#         ax.annotate('{}'.format(height),
-#                     xy=(rect.get_x() + rect.get_width() / 2, height),
-#                     xytext=(0, 3),  # 3 points vertical offset
-#                     textcoords="offset points",
-#                     ha='center', va='bottom')
+    plt.subplot(2, 2, 3)
+    for key,value in merge.items():
+        i = 0
+        for val in value:
+            if val < 0:
+                val -= 30 * i 
+                i += 1
+            plt.scatter(key,val)
+    # plt.yscale("symlog")
+    plt.title("merge")
+    plt.axhline(y=0,linewidth=1, color='black',label="zero")
+    plt.ylabel("T in seconds")
+    # plt.xticks([10,30,50,80])
+    plt.ylim(-150, 400)
 
-# autolabel(rects1)
-# autolabel(rects2)
-# autolabel(rects3)
-# autolabel(rects4)
+    plt.xlabel("Inputs, Outputs")
 
+
+    plt.subplot(2, 2, 4)
+    for key,value in split.items():
+        i = 0
+        for val in value:
+            if val < 0:
+                val -= 30 * i 
+                i += 1
+            plt.scatter(key,val)
+    # plt.yscale("symlog")
+    plt.title("split")
+    plt.axhline(y=0,linewidth=1, color='black',label="zero")
+    plt.ylabel("T in seconds")
+    # plt.xticks([10,30,50,80])
+    plt.ylim(-150, 400)
+
+    plt.xlabel("Inputs, Outputs")
+
+
+
+
+    
+    plt.tight_layout()
+
+    plt.savefig(name.format(number) + "scatter.png")
+
+if __name__ == "__main__":
+    make_figure(number=1)
+    make_scatter(number=1)
+
+    make_figure(number=2)
+    make_scatter(number=2)
+
+    make_figure(number=3)
+    make_scatter(number=3)
+
+    make_figure(number=4)
+    make_scatter(number=4)
